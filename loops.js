73e135b768148ac390045d7602667abd4d8449e1
R Stough <twlst@programmer.net>
2017-06-16 22:24:39 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,9 +1,11 @@
-'use strict';
-
 function forLoop(array) {
-  for(var i = 1; i < 25; i++) {
-    array.push(`I am ${i} strange loops.`);
+  for (let i = 0; i < 25; i++) {
+    if (i === 1)
+      array.push("I am 1 strange loop.")
+    else
+      array.push(`I am ${i} strange loops.`)
   }
+  return array
 }
 
 function whileLoop(n) {
